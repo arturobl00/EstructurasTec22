eecd1cff4c6786e886390a196c98274a6254a87c
arturobl00 <arturobl00@msn.com>
2022-08-16 13:06:18 -0500

    Clase2

---
+++ semana1/valores.js
@@ -0,0 +1,22 @@
+//Valores Primitivos usando typeof
+
+//Valor Numero Integer
+10
+//Valor de Numero Float
+13.14
+//Valor Cadena o String
+'Hola'
+"Hola2"
+//Valores Boolean o Booleanos
+true
+false
+//Valores en espera de solo con espacio de memoria
+null
+undefined
+
+//Valores Tipo objeto
+//Array
+[10,20,30,40,50]
+
+let frutas = ["Manzana", "Banana"]
+console.log(frutas)
