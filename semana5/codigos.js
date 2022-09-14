e9d6cedd0e658e1e6e037d80ff37d7127a616b62
arturobl00 <arturobl00@msn.com>
2022-09-14 10:07:56 -0500

    Actualizacion

--- semana5/codigos.js
+++ semana5/codigos.js
@@ -10,6 +10,16 @@ function agregar(){
         //console.log("Si tiene datos");
         alert("Registro Agregado con !Exito!");
         peliculas.push(titulo);
+        mostrar();
     }
 }
 
+function mostrar(){
+    document.getElementById("contenido").innerHTML = ""
+    peliculas.forEach(function(elemento, index){
+        document.getElementById("contenido").innerHTML += "<tr><td>" + 
+        100 + index + "</td><td>" + elemento + 
+        "</td><td><button class = 'btn btn-danger'>" + 
+        "ELIMINAR</button></td><td><button class = 'btn btn-warning'>EDITAR</button></td></tr>"
+    });
+}
